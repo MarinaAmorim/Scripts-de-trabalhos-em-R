657b91b9cad68ceae1dd66cc8e843f1add0fabeb
m2013034835 <m2013034835@gmail.com>
2017-06-23 13:29:49 -0300

    modelo com o botão ajustar

--- ra-2.R
+++ ra-2.R
@@ -8,7 +8,7 @@
 source("http://www.math.ntnu.no/inla/givemeINLA.R")
 require(INLA)
 require(shiny)
-
+{
 ui <- fluidPage(
   headerPanel("TRABALHO PRÁTICO - R AVANÇADO"),
   sidebarPanel(
@@ -17,12 +17,11 @@ ui <- fluidPage(
     checkboxInput("header", "Arquivo com título", TRUE),
     
     uiOutput(outputId = "varResp"),
-    uiOutput(outputId = "varExp")
-    # selectInput('resp', "Variável Resposta:", choices = ""),
-    # checkboxGroupInput('exp', "Variáveis Explicativas:", choices = "")
-  ),
+    uiOutput(outputId = "varExp"),
+    uiOutput(outputId = "ana")  
+    ),
   mainPanel(
-    # tableOutput("res0ults")
+     tableOutput("result")
   )
 )
 
@@ -34,13 +33,11 @@ server <- function(input, output, session) {
       return(NULL)
     dados <- read.table(dados$datapath, header = input$header)
   })
-  
   output$varResp <- renderUI({
     dados <- loadData()
     variaveis <- names(dados)
     selectInput('resp', "Variável Resposta:", choices = variaveis)
   })  
-  
   output$varExp <- renderUI({
     dados <- loadData()
     if (is.null(dados))
@@ -49,10 +46,22 @@ server <- function(input, output, session) {
     y_index <- which(names(dados) == y)
     variaveis2 <- names(dados)[-y_index]	
     checkboxGroupInput('exp', "Variáveis Explicativas:", choices = variaveis2)
+    
   })
+  output$ana<- renderUI({actionButton("analysis","Ajustar") })
   
   
+  observeEvent(input$analysis, {
+  output$result <- renderPrint({
+    dados <- loadData()
+    model=lm(input$resp ~ input$exp, data =dados )
+      summary(model)
+    })
+  })
 }
+
+
 shinyApp(ui = ui, server = server)
+}
 
 
