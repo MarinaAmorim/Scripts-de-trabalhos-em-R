bf71a608b2dd021f96dac09cc4a35bfa2409ca95
m2013034835 <m2013034835@gmail.com>
2017-06-28 13:42:32 -0300

    Rodando  tudo ok
    Falta conferir se o grafico de pit esta certo

--- ra-2.R
+++ ra-2.R
@@ -1,67 +1,93 @@
 #####################################################
-# Trabalho Pr·tico
+# TRABALHO PR¡TICO
 #####################################################
 
-##PACOTES NECESS¡RIOS
-
-# Para instalar o pacote INLA È necess·rio usar o comando abaixo
+##PACOTES NECESS√ÅRIOS
+# Para instalar o pacote INLA √© necess√°rio usar o comando abaixo
 source("http://www.math.ntnu.no/inla/givemeINLA.R")
 require(INLA)
 require(shiny)
-{
+
+
 ui <- fluidPage(
   headerPanel("TRABALHO PR¡TICO - R AVAN«ADO"),
   sidebarPanel(
+    selectInput("separador","Como as colunas do seu arquivo s„o separadas?", choices = c("tab","espaÁo",",",";"), selected = "tab"),
     fileInput("dados","Escolha um arquivo .txt",multiple = FALSE,accept = ".txt",buttonLabel = "Ler"),
     tags$hr(),
-    checkboxInput("header", "Arquivo com tÌtulo", TRUE),
-    
+    checkboxInput("header", "Arquivo com tÌtulo", TRUE),    
     uiOutput(outputId = "varResp"),
     uiOutput(outputId = "varExp"),
-    uiOutput(outputId = "ana")  
-    ),
+    actionButton("modelar", "Gerar saÌdas")
+  ),
   mainPanel(
-     tableOutput("result")
+    tabsetPanel(
+      tabPanel("Summary", 
+               verbatimTextOutput("summary")
+      ),
+      
+      tabPanel("Valores Ajustados",
+               verbatimTextOutput("fitted")
+      ),
+      tabPanel("Gr·fico de PIT",
+               plotOutput("pit")
+      )
+    )
   )
 )
 
 server <- function(input, output, session) {
   
   loadData <- reactive({
+    if(input$separador == "tab")
+      sep = "\t"
+    else if (input$separador == "espaÁo")
+      sep = ""
+    else 
+      sep = input$separador
     dados <- input$dados
     if (is.null(dados))
       return(NULL)
-    dados <- read.table(dados$datapath, header = input$header)
+    dados <- read.table(dados$datapath, header = input$header, sep = sep)
   })
+  
   output$varResp <- renderUI({
     dados <- loadData()
     variaveis <- names(dados)
     selectInput('resp', "Vari·vel Resposta:", choices = variaveis)
   })  
+  
   output$varExp <- renderUI({
     dados <- loadData()
     if (is.null(dados))
       return(NULL)
     y <- input$resp
     y_index <- which(names(dados) == y)
-    variaveis2 <- names(dados)[-y_index]	
+    variaveis2 <- names(dados)[-y_index] 
     checkboxGroupInput('exp', "Vari·veis Explicativas:", choices = variaveis2)
-    
   })
-  output$ana<- renderUI({actionButton("analysis","Ajustar") })
-  
-  
-  observeEvent(input$analysis, {
-  output$result <- renderPrint({
+  observeEvent( input$modelar, {
     dados <- loadData()
-    model=lm(input$resp ~ input$exp, data =dados )
+    if (is.null(dados))
+      return(NULL)
+    
+    y <- input$resp
+    x <- input$exp
+    model=lm(as.formula(paste(input$resp," ~ ",paste(input$exp,collapse="+"))),data=dados)
+    
+    output$summary <- renderPrint({
       summary(model)
+    })
+    
+    output$fitted <- renderPrint({
+      as.matrix(model$fitted.values)
+    })
+    
+    output$pit <- renderPlot({
+      inla.ks.plot(as.vector(model$residuals), pnorm) 
     })
   })
 }
-
-
 shinyApp(ui = ui, server = server)
-}
 
 
